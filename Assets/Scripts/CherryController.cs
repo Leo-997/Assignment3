a8106287fc66937f03f655138c53dc9854337cde
Leo-997 <489366635@qq.com>
2022-10-19 21:24:30 +0800

    70% move cherry

--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -10,36 +10,40 @@ public class CherryController : MonoBehaviour
     public GameObject test;
     public Vector2 newPos;
     private Tweener tweener;
+    private float localTime;
     private void Start()
     {
         tweener = GetComponent<Tweener>();
     }
     void Update()
     {
+        localTime += Time.deltaTime;
+        if (localTime > gapTime)
+        {
+            creatPrefab = true;
+            gapTime += 10;
+            Creat();
+        }
+        if (test)
+        {
+            if (localTime > gapTime - 1)
+            {
+                Destroy(test);
+            }
+        }
 
+    }
+    public void Creat()
+    {
         if (creatPrefab)
         {
             float x = Random.Range(-27, -35);
             float y = Random.Range(14, -15);
             Vector2 pos = new Vector2(x, y);
             test = Instantiate(prefab, pos, Quaternion.identity);
-            newPos = new Vector2(-test.transform.position.x, -test.transform.position.y);
+            newPos = -pos;
             tweener.AddTween(test.transform, test.transform.position, newPos, 8.5f);
             creatPrefab = false;
         }
-        if (Time.time > gapTime)
-        {
-            creatPrefab = true;
-            gapTime += 10;
-
-        }
-        if (test)
-        {
-            if (Time.time > gapTime - 1)
-            {
-                Destroy(test);
-            }
-        }
-
     }
 }
